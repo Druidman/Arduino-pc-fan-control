f077d64f4105c227234938994703501b15d6a135
Mateusz Kowalski <towermanxxl@gmail.com>
2025-02-03 21:46:02 +0100

    basic styles

--- driver/app/main/main.js
+++ driver/app/main/main.js
@@ -4,8 +4,8 @@ const { setupIpcHandler }  = require("./ipcHandler.js")
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
         preload: path.join(__dirname, 'preload.js')
     }
